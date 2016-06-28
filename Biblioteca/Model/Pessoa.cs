d771a8224219bc13a627286329060862a2ea6c56
estevaogut <estevaorg@gmail.com>
2016-06-28 20:30:42 -0300

    v1.9

---
+++ Biblioteca/Model/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Model
+{
+    [Table("Pessoas")]
+    class Pessoa
+    {
+        [Key]
+        public int IdPessoa { get; set; }
+        public string Nome { get; set; }
+        public string DataNasc { get; set; }
+        public string Telefone { get; set; }
+    }
+}

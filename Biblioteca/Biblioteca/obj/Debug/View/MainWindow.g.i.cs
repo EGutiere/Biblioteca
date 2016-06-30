bc849fee99227018efaef27f374a5831de8aeba9
João Filipe <joaoflyper@gmail.com>
2016-06-30 10:30:53 -0300

    Biblioteca v3.1

--- Biblioteca/Biblioteca/obj/Debug/View/MainWindow.g.i.cs
+++ Biblioteca/Biblioteca/obj/Debug/View/MainWindow.g.i.cs
@@ -39,6 +39,14 @@ namespace Biblioteca {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 6 "..\..\..\View\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Button button;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -67,6 +75,12 @@ namespace Biblioteca {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.button = ((System.Windows.Controls.Button)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
